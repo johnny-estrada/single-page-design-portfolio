5b382e3ed2cb13f050ac0b37054cdd71973a3daf
Johnny Estrada <johnnye562@yahoo.com>
2022-11-12 01:32:35 -0800

    Create Navbar and styles

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <header className='header'>
+            <img className='header__img' src="../public/images/logo.svg" alt='logo' />
+            <button className='header__btn'>Free Consultation</button>
+        </header>
+    )
+}
+
+export default Navbar;
