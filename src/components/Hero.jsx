7ca16c933a6f14fb6b1364b7410beb9354cf756d
Johnny Estrada <johnnye562@yahoo.com>
2022-11-14 01:05:46 -0800

    Create Hero section and styles

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import './Hero.css'
+
+
+function Hero() {
+
+        return (
+            <section className='hero'>
+                <h1 className='hero__title'>Design solutions made easy</h1>
+                <p className='hero__text'>With over ten years of experience in various design disciplines, I'm your one-stop shop for your design needs.</p>
+                <div className='boxes'>
+                    <img />
+                </div>
+            </section>
+        )
+
+}
+
+export default Hero;
