2e29ac6070c403dc26649b1dda0eb958db3b66f4
Johnny Estrada <johnnye562@yahoo.com>
2022-11-21 15:20:30 -0800

    Update Navbar style and add link to logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,10 @@ import './Navbar.css'
 function Navbar() {
     return (
         <header className='header'>
+        <a href='#'>
             <img className='header__img' src="./images/logo.svg" alt='logo' />
-            <button className='header__btn'>Free Consultation</button>
+        </a> 
+            <button className='header__cta'>Free Consultation</button>
         </header>
     )
 }
