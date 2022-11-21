16e245faca7c90be79b44179f0bcddf9b8720540
Johnny Estrada <johnnye562@yahoo.com>
2022-11-21 14:16:32 -0800

    Correct src for img in navbar log

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css'
 function Navbar() {
     return (
         <header className='header'>
-            <img className='header__img' src=".images/logo.svg" alt='logo' />
+            <img className='header__img' src="./images/logo.svg" alt='logo' />
             <button className='header__btn'>Free Consultation</button>
         </header>
     )
